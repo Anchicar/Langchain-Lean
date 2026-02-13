3751f3a5ff26e04bfd63da0f011a02fb1109042d
Ander31416 <graminoplo@gmail.com>
2026-02-12 21:15:40 -0500

    Structure set up

---
+++ langchain_lean/core/evaluator.py
@@ -0,0 +1,11 @@
+'''
+Crear una función que como input tenga código lean y que dentro corra el código con ayuda de
+un contenedor Docker (Dockerfile), dando sasi como return la respuesta del código lean
+ejecutado.
+
+Posiblemente se vayan a tener que utilizar los archivos setup-env.sh y enviroment.py para asi hacer
+la respectiva configuración de docker y el entorno donde correrá el código
+'''
+
+def run_lean_code(lean_code : str):
+    return None
